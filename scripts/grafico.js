3ddf583d49ba051b4e1fb54ecb1170adc1d0f747
Saralopezlovon <lopezlovonsara@gmail.com>
2021-10-07 14:12:34 +0200

    Add first grafic1 with js

--- scripts/grafico.js
+++ scripts/grafico.js
@@ -41,7 +41,25 @@ const drawGrafic1 =  () => {
     getDataStarWars()
 
     .then (objDatos=>{
-        console.log(objDatos)
+
+        let data = {
+            // A labels array that can contain any sort of values
+            labels: objDatos.tituloPelicula ,
+            // Our series array that contains series objects or in this case series data arrays
+            series: [objDatos.añoPublicacion]
+          };
+
+          let options = {
+            width: 700,
+            height: 500
+          };
+          
+          // Create a new line chart object where as first parameter we pass in a selector
+          // that is resolving to our chart container element. The Second parameter
+          // is the actual data object.
+        new Chartist.Line('.ct-chart', data, options);
+        
+        
     })
     
     
